fd67076ea33e0d516daf56797dd10815b97ca190
mauriciocv22 <117070016+mauriciocv22@users.noreply.github.com>
2022-11-12 12:32:26 -0600

    Add files via upload

---
+++ Comeximtto/js/index.js
@@ -0,0 +1,88 @@
+window.sr = ScrollReveal();
+sr.reveal('.container__header', {
+    duration: 1000,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+  sr.reveal('.titulo', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+  sr.reveal('h2', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.container__info', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.container__img', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '-300px'
+  });
+  sr.reveal('.container__img2', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '-300px'
+  });
+
+  sr.reveal('.columna1', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+  sr.reveal('.columna2', {
+    duration: 2500,
+    origin: 'top',
+    distance: '-100px'
+  });
+
+  sr.reveal('.button', {
+    duration: 3000,
+    origin: 'rigth',
+    distance: '-100px'
+  });
+
+
+  sr.reveal('.container__footer', {
+    duration: 1000,
+    origin: 'top',
+    distance: '-100px'
+  });
+  
+  sr.reveal('.fb', {
+    duration: 3000,
+    origin: 'rigth',
+    distance: '-50px'
+  });
+  
+  sr.reveal('.inst', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-50px'
+  });
+
+  sr.reveal('.twit', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-50px'
+  });
+
+  sr.reveal('.whats', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-50px'
+  });
+
+  sr.reveal('.ubi', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-50px'
+  });
